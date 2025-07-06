fc5eda350d38c79171a73dd88a1709bf8f5f1a44
Xavier Sanz <xavier.sanz@ebrook.com.tw>
2025-07-06 15:24:26 +0800

    Adding update-cobj route and pug template

--- index.js
+++ index.js
@@ -19,9 +19,17 @@ const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
 
 // * Code for Route 1 goes here
 
+app.get('/', (req, res) => {
+    //NOTHING TO DO HERE AT THE MOMENT
+});
+
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
 // * Code for Route 2 goes here
+app.get('/update-cobj', async (req, res) => {
+    res.render('updates', {title: 'Update Custom Object Form | Integrating With HubSpot I Practicum'});
+});
+
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
